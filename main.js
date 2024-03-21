a5327f71641c0e25beb49c282fd8c7ad14690979
Daniyal Khan <159725508+Danii96khan@users.noreply.github.com>
2024-03-22 03:58:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//step 01 Choices through Inquirer
+const answers = await inquirer.prompt([
+    { type: "number",
+        name: "numberOne",
+        message: "Enter your first number"
+    },
+    { type: "number",
+        name: "numberTwo",
+        message: "enter your second number"
+    },
+    { type: "list",
+        name: "operator",
+        message: "enter your operator",
+        choices: ["+", "-", "*", "/"] }
+]);
+//Step02 USe Switch Case
+const { numberOne, numberTwo, operator } = answers;
+let result;
+switch (operator) {
+    case "+":
+        result = numberOne + numberTwo;
+        break;
+    case "-":
+        result = numberOne - numberTwo;
+        break;
+    case "*":
+        result = numberOne * numberTwo;
+        break;
+    case "/":
+        result = numberOne / numberTwo;
+        break;
+    default:
+        console.log("invalid operator");
+}
+console.log(`${numberOne} ${operator} ${numberTwo} =${result}`);
